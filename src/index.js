b1a59ddb787159c1c459f54490026576fb10bdd6
Jungea <jungea2241@gmail.com>
2021-06-28 13:02:04 +0900

    Square 컴포넌트에 state 사용

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square" onClick={() => { alert('click'); }}>
-        {this.props.value}
+      <button
+        className="square"
+        onClick={() => {
+          this.setState({ value: "X" });
+        }}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
-  
+
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;  
+    return <Square value={i} />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = "Next player: X";
 
     return (
       <div>
@@ -61,7 +73,4 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById("root"));
